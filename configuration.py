235d6d3631abfdb1683a0671f327590487517735
Jungerdurst862 <121775185+Jungerdurst862@users.noreply.github.com>
2024-12-07 23:29:24 +0100

    now ready to release

--- configuration.py
+++ configuration.py
@@ -23,14 +23,14 @@ Amount_of_new_Anime_Download_new = 150
 
 
 #JDownloader Configurations ---------------------------------------------------------------------------------------------------------------------------
-JDownloader_device = "JDownloader@paull"#Write your device name from JDownloader
-email = "paul.lennard.kessel@icloud.com"#wirte your JD email
-password = "PaLeKe6280!"#write your Jd Password
+JDownloader_device = ""#Write your device name from JDownloader
+email = ""#wirte your JD email
+password = ""#write your Jd Password
 
 
 #Discord Configuration----------------------------------------------------------------------------------------------------------------------------------
 Use_Discord = False #if enable it will send discord messages
-Webhock = "https://discord.com/api/webhooks/1312418426010079283/W1fCaDubmH-VReHw9L2z0v_CCcHzdf67FAx7gR3QTtH8m05apPGD072412QuLWU8NFId"#Channel Webhook URL
-autherization = "NjkxOTgxMzMxNDc0ODA4ODMz.GC84UC.dO7Wygw8viKZVH1H012usXszsYZpOQv1E8-LuI"#your authorization for discord.com
-Channel_id = "1312418404451356773"#channel id where you want to send the message
+Webhock = ""#Channel Webhook URL
+autherization = ""#your authorization for discord.com
+Channel_id = ""#channel id where you want to send the message
 wait_time = 60  #wait time before stop waiting for a response from discord
